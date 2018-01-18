e7b6bdcff7a194cee3e17cabf9c6f738b978e318
Pierre-Olivier Quirion <pioliqui@gmail.com>
2018-01-18 15:09:22 -0500

    rewrite sct_config.py

--- scripts/sct_config.py
+++ scripts/sct_config.py
@@ -4,30 +4,16 @@ import logging
 import subprocess
 
 
-
-def _get_sct_version():
+def __get_sct_version():
     """Get git commit if in repo and version for the version.txt file
 
     :return: Tuple (install_type, sct_commit, sct_branch, version_sct)
     """
-    sct_commit = 'unknown'
-    sct_branch = 'unknown'
+    sct_commit = __git_commit__
+    sct_branch = __git_branch__
 
-    if os.path.isdir(os.path.join(__sct_dir__, '.git')):
+    if sct_commit is not 'unknown':
         install_type = 'git'
-        p = subprocess.Popen(["git", "rev-parse", "HEAD"], stdout=subprocess.PIPE, stderr=subprocess.PIPE,
-                                         cwd=__sct_dir__)
-        output, _ = p.communicate()
-        status = p.returncode
-        if status == 0:
-            sct_commit = output
-        p = subprocess.Popen(["git", "rev-parse", "--abbrev-ref", "HEAD"], stdout=subprocess.PIPE,
-                                         stderr=subprocess.PIPE, cwd=__sct_dir__)
-        output, _ = p.communicate()
-        status = p.returncode
-
-        if status == 0:
-            sct_branch = output
     else:
         install_type = 'package'
 
@@ -37,11 +23,43 @@ def _get_sct_version():
     return install_type, sct_commit, sct_branch, version_sct
 
 
+def __get_branch():
+    """
+    Fallback if for some reason the value vas no set by sct_launcher
+    :return:
+    """
+
+    p = subprocess.Popen(["git", "rev-parse", "--abbrev-ref", "HEAD"], stdout=subprocess.PIPE,
+                         stderr=subprocess.PIPE, cwd=__sct_dir__)
+    output, _ = p.communicate()
+    status = p.returncode
+
+    if status == 0:
+        return output
+    return 'unknown'
+
+
+def __get_commit():
+    """
+    Fallback if for some reason the value vas no set by sct_launcher
+    :return:
+    """
+    p = subprocess.Popen(["git", "rev-parse", "HEAD"], stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+                         cwd=__sct_dir__)
+    output, _ = p.communicate()
+    status = p.returncode
+    if status == 0:
+        return output
+    return 'unknown'
+
+
 # Basic sct config
 __sct_dir__ = os.getenv("SCT_DIR", os.path.dirname(os.path.realpath(__file__)).rstrip("scripts"))
 __data_dir__ = os.getenv("SCT_DATA_DIR", "{}/data".format(__sct_dir__))
-__version__ = '-'.join(_get_sct_version())
-
+# Be careful no to change the order commit, branch and then version!
+__git_commit__ = os.getenv("SCT_COMMIT", __get_commit())
+__git_branch__ = os.getenv("SCT_BRANCH", __get_branch())
+__version__ = '-'.join(__get_sct_version())
 
 # statistic report level
 __report_log_level__ = logging.ERROR
